42eafcb5d93dd0ba574b1beadf44dc0acc6da8d8
MFJWR <chanpie2007@gmail.com>
2017-04-04 13:42:14 +0900

    Improve readability

--- .hammerspoon/sticky.lua
+++ .hammerspoon/sticky.lua
@@ -2,22 +2,15 @@
 local sticky = false
 
 local function handleSticky(e)
-
-    local keyCode = e:getKeyCode()
-    local flags = e:getFlags()
-
-    if keyCode == 41 then -- セミコロン
-        if flags['shift'] or sticky then -- シフト付き or sticky=true
-            sticky = false
-        else -- セミコロン単独
-            e:setKeyCode(-1)
-            sticky = true
-        end
-    else -- セミコロン以外
+    if e:getKeyCode() == 41 then -- セミコロン
+        local flags = e:getFlags()
+        sticky = not(sticky) and not(flags['shift'])
         if sticky then
-            e:setFlags({shift=true})
-            sticky = false
+            e:setKeyCode(-1)
         end
+    elseif sticky then -- stickyが有効の時にセミコロン以外のキーが入力された
+        e:setFlags({shift=true})
+        sticky = false
     end
 end
 
